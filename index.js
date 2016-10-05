1d4decca6358bdf81ab4b8e9b608a4c36febc722
Tapan Halani <tapanhalani231@gmail.com>
2016-10-05 15:15:25 +0530

    log changed.

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ http.createServer(function (request, response) {
 }).listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081, go there and check it../');
