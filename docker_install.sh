9c3483c902638b45ab0a6630d9f3e81891d52116
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 19:09:15 +0530

    changed repository name.

--- docker_install.sh
+++ docker_install.sh
@@ -6,4 +6,4 @@ tar zcf node-hello-world.tar.gz .
 
 curl -v  --unix-socket /var/run/docker.sock -H 'Content-Type: application/tar' --data-binary @node-hello-world.tar.gz  -X POST http:/build?t=tapanhalani/node-hello-world:latest
 
-curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: { "username": "tapanhalani", "password": "28march1994", "email": "tapanhalani231@gmail.com" }' -X POST http:/images/node-hello-world/push?tag=latest
+curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: { "username": "tapanhalani", "password": "28march1994", "email": "tapanhalani231@gmail.com" }' -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
