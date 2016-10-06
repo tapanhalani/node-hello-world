0039801306a9ae8fc80e1e6e33e3ca147acd00fd
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 21:04:46 +0530

    final change.

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENCODED_OBJ=`base64 docker_hub_auth.txt | sed 's/ //g'`
-echo $ENCODED_OBJ
-echo "tapan halani is working" | sed 's/ //g'
-curl -v  --unix-socket /var/run/docker.sock -H "X-Registry-Auth:$ENCODED_OBJ" -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
+ENCODED_OBJ=`base64 docker_hub_auth.txt`
+FINAL_OBJ="$(echo -e "${ENCODED_OBJ}" | tr -d '[[:space:]]')"
+echo -e "FINAL_OBJ='${FINAL_OBJ}'"
+curl -v  --unix-socket /var/run/docker.sock -H "X-Registry-Auth:$FINAL_OBJ" -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
