a19efe32d0d8f47df818e91c2fb135346d5e9359
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 19:56:38 +0530

    final change.

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 
 ENCODED_OBJ=`base64 docker_hub_auth.txt`
 echo $ENCODED_OBJ
-curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: $ENCODED_OBJ' -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
+curl -v  --unix-socket /var/run/docker.sock -H "X-Registry-Auth: $ENCODED_OBJ" -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
