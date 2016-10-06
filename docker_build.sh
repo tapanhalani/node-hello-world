18142ceb011e4cc0f833385d4c73fad2ff113f8c
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 19:48:40 +0530

    error in push stage resolved.

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,4 @@
-tar zcf node-hello-world.tar.gz .
+#!/bin/bash
 
+tar zcf node-hello-world.tar.gz .
 curl -v  --unix-socket /var/run/docker.sock -H 'Content-Type: application/tar' --data-binary @node-hello-world.tar.gz  -X POST http:/build?t=tapanhalani/node-hello-world:latest
--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
-ENCODED_OBJ = `base64 docker_hub_auth.txt`
+#!/bin/bash
 
+ENCODED_OBJ=`base64 docker_hub_auth.txt`
+echo $ENCODED_OBJ
 curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: $ENCODED_OBJ' -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
