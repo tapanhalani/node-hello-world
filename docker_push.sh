cdf3acbec1b59d300f96985ac8a82ae7ae6f8d1d
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 20:30:58 +0530

    trim white-space in header.

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ENCODED_OBJ=`base64 docker_hub_auth.txt`
+ENCODED_OBJ=`base64 docker_hub_auth.txt | sed 's/ //g'`
 echo $ENCODED_OBJ
 curl -v  --unix-socket /var/run/docker.sock -H "X-Registry-Auth: $ENCODED_OBJ" -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
