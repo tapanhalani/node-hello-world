ce9f4d58ec8f035a43b4868a2bbb93925d0ef972
Tapan Halani <tapanhalani231@gmail.com>
2016-10-06 21:18:24 +0530

    reverting to previous code...

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: eyB1c2VybmFtZTogdGFwYW5oYWxhbmksIHBhc3N3b3JkOiAyOG1hcmNoMTk5NCwgZW1haWw6IHRhcGFuaGFsYW5pMjMxQGdtYWlsLmNvbSB9Cg' -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
+curl -v  --unix-socket /var/run/docker.sock -H 'X-Registry-Auth: eyB1c2VybmFtZTogdGFwYW5oYWxhbmksIHBhc3N3b3JkOiAyOG1hcmNoMTk5NCwgZW1haWw6IHRhcGFuaGFsYW5pMjMxQGdtYWlsLmNvbSB9Cg==' -X POST http:/images/tapanhalani/node-hello-world/push?tag=latest
